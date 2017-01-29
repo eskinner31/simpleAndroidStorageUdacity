e4232f10fe8668b5a40395789ad4c6aa4451280b
Eugene Skinner <eugene.skinner@homes.com>
2017-01-29 15:25:20 -0500

    Initial commit, added Habit Contract and HabitDbHelper

---
+++ app/src/main/java/com/example/android/habittracker/data/HabitContract.java
@@ -0,0 +1,38 @@
+package com.example.android.habittracker.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Skinner on 1/29/17.
+ */
+
+public class HabitContract {
+
+    public final static String TAG = HabitContract.class.getSimpleName();
+
+    private HabitContract() {};
+
+    public final static class HabitEntry implements BaseColumns {
+
+        //Would it still be acceptable to utilize strings.xml for the values here?
+        public final static String TABLE_NAME = "tracked_habits";
+
+        //Are there rules for the ordering of "final" and "static" when declaring a variable.
+        public final static String _ID = BaseColumns._ID;
+        public final static String COLUMN_HABIT_NAME = "name";
+        public final static String COLUMN_HABIT_IMPORTANCE = "importance";
+        public final static String COLUMN_HABIT_REPETITIONS = "repetitions";
+        public final static String COLUMN_HABIT_ALERT = "alert";
+
+        //Values for importance
+        public final static int IMPORTANCE_LOW = 0;
+        public final static int IMPORTANCE_MODERATE = 1;
+        public final static int IMPORTANCE_HIGH = 2;
+        public final static int IMPORTANCE_UBER_HIGH = 3;
+
+        //Values for alert
+        public final static int ALERT_FALSE = 0;
+        public final static int ALERT_TRUE = ;
+    }
+
+}
---
+++ app/src/main/java/com/example/android/habittracker/data/HabitDbHelper.java
@@ -0,0 +1,39 @@
+package com.example.android.habittracker.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.example.android.habittracker.data.HabitContract.HabitEntry
+
+/**
+ * Created by Skinner on 1/29/17.
+ */
+
+public class HabitDbHelper extends SQLiteOpenHelper {
+
+    public static final String TAG = HabitDbHelper.class.getSimpleName();
+
+    public static final String DATABASE_NAME = "habit.db";
+    public static final int DATABASE_VERSION = 1;
+
+    public HabitDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        String SQL_CREATE_HABITS_TABLE = "CREATE TABLE " + HabitEntry.TABLE_NAME + " ("
+                + HabitEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + HabitEntry.COLUMN_HABIT_NAME + " TEXT NOT NULL, "
+                + HabitEntry.COLUMN_HABIT_IMPORTANCE + " INTEGER NOT NULL, "
+                + HabitEntry.COLUMN_HABIT_ALERT + " INTEGER NOT NULL, "
+                + HabitEntry.COLUMN_HABIT_REPETITIONS + " INTEGER NOT NULL DEFAULT 1);";
+
+        sqLiteDatabase.execSQL(SQL_CREATE_HABITS_TABLE);
+    }
+
+    //Implemented method, No upgrade code since version will remain one.
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {}
+}
